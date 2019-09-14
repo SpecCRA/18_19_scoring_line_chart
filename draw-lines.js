d5df4f8b32063b90deaa7a01ce91141fc40943c0
SpecCRA <benjamin.y.xiao@gmail.com>
2019-09-14 05:03:03 -0700

    Added: y-axis label, title. Changed: line color. To do: variance shading.

--- draw-lines.js
+++ draw-lines.js
@@ -6,11 +6,12 @@ async function drawLines() {
     xAccessor = d => d.date;
 
     console.log(d3.min(dataset, yAccessor))
+    console.log(d3.deviation(dataset, yAccessor))
 
     // Chart dimensions
     let dimensions = {
         width: window.innerWidth * 0.9,
-        height: 400,
+        height: 450,
         margin: {
             top: 15,
             right: 15,
@@ -58,7 +59,7 @@ async function drawLines() {
     const line = bounds.append("path")
         .attr("d", lineGenerator(dataset))
         .attr("fill", "none")
-        .attr("stroke", "black")
+        .attr("stroke", "#0d6647")
         .attr("stroke-width", 2)
     
     // Draw peripherals
@@ -66,7 +67,17 @@ async function drawLines() {
         .scale(yScale)
         .tickValues([100,120,160])
     const yAxis = bounds.append("g")
-        .call(yAxisGenerator)
+		.call(yAxisGenerator)
+		.style("color", "#878787")
+
+	const yAxisLabel = yAxis.append("text")
+		.attr("x", dimensions.width * 0.075)
+		.attr("y", dimensions.height * 0.05)
+		.attr("fill", "black")
+		.style("font-size", "1.4em")
+		.text("Total Points Scored per Game")
+		// .style("transform", "rotate(-90deg)")
+		.style("text-anchor", "middle")
     
     const xAxisGenerator = d3.axisBottom()
         .scale(xScale)
@@ -74,7 +85,23 @@ async function drawLines() {
         .call(xAxisGenerator)
             .style("transform", `translateY(${
                 dimensions.boundedHeight
-            }px)`)
+			}px)`)
+		.style("color", "#878787")
+
+	// const xAxisLabel = xAxis.append("text")
+	// 	.attr("x", dimensions.boundedWidth * 0.985)
+	// 	.attr("y", dimensions.margin.bottom - 25)
+	// 	.attr("fill", "black")
+	// 	.attr("font-size", "1.4em")
+	// 	.text("Date")
+	
+	// Title
+	const title = wrapper.append("text")
+		.attr("x", (dimensions.width * 0.8))
+		.attr("y", (dimensions.height * 0.05))
+		.attr("text-anchor", "middle")
+		.style("font-size", "16px")
+		.text("The 2018-19 Boston Celtics had one of the most disgruntled teams of the season.")
     
 }
 
